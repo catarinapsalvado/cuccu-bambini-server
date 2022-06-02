a9cae546a13201435e95064ed70ce37b284fef8b
Catarina Salvado <catarinapsalvado@gmail.com>
2022-06-02 10:08:25 +0100

    User and product models

--- models/User.model.js
+++ models/User.model.js
@@ -1,18 +1,35 @@
 const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema(
-  {
+const userSchema = new Schema({
+    email: { 
+      type: String, 
+      required: true,
+       unique: true },
+
     username: {
-      type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
-    },
-    password: String,
+    type: string, 
+    required: true
   },
-  {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
-    timestamps: true,
-  }
+    password: {
+       type: String, 
+       required: true },
+
+    address: {
+      type: String,
+      default: ''
+    },
+
+    role: {
+      type: String, //user, admin
+      default: 'user',
+    },
+
+    cart: {
+      type: Array,
+      default: [] 
+    }
+  }  
 );
 
 const User = model("User", userSchema);
